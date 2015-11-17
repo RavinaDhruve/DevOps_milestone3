b720d807758f4262ca94233e3d7f90c2030215ca
Ravina Dhruve <Ravina@Ravinas-MacBook-Pro.local>
2015-11-16 23:26:52 -0500

    redis store

---
+++ redis_store.js
@@ -0,0 +1,19 @@
+var redis = require('redis');
+
+var client = redis.createClient(6379, '127.0.0.1', {});
+
+// cleaning for debugging purpose
+//client.del('Prod');
+//client.del('Canary');
+
+client.hmset('Prod', {
+        '/': 'True',
+        '/about': 'False'
+});
+
+client.hmset('Canary', {
+        '/': 'True',
+        '/about': 'True'
+});
+
+console.log("Values set in REDIS store.");
