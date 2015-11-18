9630e78625c52e17a5c9742fb7febf97749b93ca
Ravina Dhruve <Ravina@Ravinas-MacBook-Pro.local>
2015-11-18 17:05:04 -0500

    redis_store

--- redis_store.js
+++ redis_store.js
@@ -8,7 +8,7 @@ var client = redis.createClient(6379, '127.0.0.1', {});
 
 client.hmset('Prod', {
         '/': 'True',
-        '/about': 'False'
+        '/about': 'True'
 });
 
 client.hmset('Canary', {
