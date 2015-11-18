b4345b8da224adeef12632a42f3954bdd905e824
Ravina Dhruve <Ravina@Ravinas-MacBook-Pro.local>
2015-11-18 16:15:33 -0500

    canary

--- setup_canary.sh
+++ setup_canary.sh
@@ -6,8 +6,8 @@ sudo docker run -td --name prodtest8 m3app:latest
 sudo docker stop $(sudo docker ps -a -f 'name=redis*' -q)
 sudo docker rm $(sudo docker ps -a -f 'name=redis*' -q)
 
-sudo docker stop $(sudo docker ps -a -f 'name=prodtest*' -q)
-sudo docker rm $(sudo docker ps -a -f 'name=prodtest*' -q)
+sudo docker stop $(sudo docker ps -a -f 'name=prodtest2' -q)
+sudo docker rm $(sudo docker ps -a -f 'name=prodtest2' -q)
 
 sudo docker run -d --name redis crosbymichael/redis
 sudo docker run -d --link redis:redis --name redis_ambassador -p 6379:6379 svendowideit/ambassador
