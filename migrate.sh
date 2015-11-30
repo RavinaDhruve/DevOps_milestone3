531172b531ef566548c993f20fee3a09737a4bff
Ravina Dhruve <Ravina@Ravinas-MacBook-Pro.local>
2015-11-29 21:05:04 -0500

    mig7

--- migrate.sh
+++ migrate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
 sudo docker run -d --name redis1 crosbymichael/redis
-sudo docker run -td --name prodtest8 m3app:latest
+sudo docker run -td --name prodtest8 ubuntu:trusty
 
 sudo docker stop $(sudo docker ps -a -f 'name=prod*' -q)
 sudo docker rm $(sudo docker ps -a -f 'name=prod*' -q)
@@ -12,7 +13,11 @@ sudo docker rm $(sudo docker ps -a -f 'name=redis*' -q)
 sudo docker run -d --name redis crosbymichael/redis
 sudo docker run -d --link redis:redis --name redis_ambassador -p 6379:6379 svendowideit/ambassador
 
-sudo docker run --link redis_ambassador:redis -v `pwd`:/server -p 3000:5000 -td --name prodtest1 m4app:latest
+sudo docker run --link redis_ambassador:redis -v `pwd`:/server -p 3000:5000 -td --name prodtest1 ubuntu:trusty
+
+sudo docker exec -td prodtest1 sh -c "sudo apt-get update"
+sudo docker exec -td prodtest1 sh -c "sudo apt-get install npm"
+sudo docker exec -td prodtest1 sh -c "npm install nodejs"
 
 sudo docker exec -td prodtest1 sh -c "nodejs server/app.js Prod 5000"
 
--- trialssh.sh
+++ trialssh.sh
@@ -3,5 +3,4 @@
 sudo apt-get install sshpass
 #sshpass -p rootroot ssh root@107.170.19.156
 
-sshpass -p 'rootroot' ssh -o StrictHostKeyChecking=no root@107.170.19.156
-mkdir server2
+sshpass -p 'rootroot' ssh -o StrictHostKeyChecking=no root@107.170.19.156 "scp root@104.131.197.251:/var/lib/jenkins/jobs/Migration_monkey/workspace/migrate.sh migrate.sh && sh migrate.sh"
