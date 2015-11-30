69d442056eecde2781e23e3dc47caf6c0ac5ffec
Ravina Dhruve <Ravina@Ravinas-MacBook-Pro.local>
2015-11-30 13:23:44 -0500

    Try mig

--- trialssh.sh
+++ trialssh.sh
@@ -5,4 +5,4 @@ sudo apt-get install sshpass
 
 #scp -r /var/lib/jenkins/jobs/Migration_monkey/workspace/new_env.sh root@107.170.19.156:new_env.sh
 
-sshpass -p 'rootroot' ssh -o StrictHostKeyChecking=no root@107.170.19.156 'git clone https://github.com/RavinaDhruve/DevOps_milestone3.git && cd DevOps_milestone3 && sh new_env.sh'
+sshpass -p 'rootroot' ssh -o StrictHostKeyChecking=no root@107.170.19.156 'git clone https://github.com/RavinaDhruve/DevOps_milestone3.git && cd DevOps_milestone3'
