21bffad9116ff6365afeeb0ae4d9936c744a9c26
Ravina Dhruve <Ravina@Ravinas-MacBook-Pro.local>
2015-11-30 00:33:02 -0500

    simple infra

--- old_env.sh
+++ old_env.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-node app.js Prod 3000 &
-
 wget http://download.redis.io/releases/redis-3.0.5.tar.gz
 tar xzf redis-3.0.5.tar.gz
 cd redis-3.0.5
 make
-
 src/redis-server &
+
+node app.js Prod 3000 &
 
 echo "DEPLOY COMPLETED SUCCESSFULLY."
