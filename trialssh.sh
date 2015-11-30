48b6662cf0fdde81e3d4f2417859bc74999da70b
Ravina Dhruve <Ravina@Ravinas-MacBook-Pro.local>
2015-11-29 20:57:44 -0500

    mig5

--- trialssh.sh
+++ trialssh.sh
@@ -3,4 +3,4 @@
 sudo apt-get install sshpass
 #sshpass -p rootroot ssh root@107.170.19.156
 
-sshpass -p 'rootroot' ssh -o StrictHostKeyChecking=no root@107.170.19.156 'echo "DEPLOY COMPLETED SUCCESSFULLY."'
+sshpass -p 'rootroot' ssh -o StrictHostKeyChecking=no root@107.170.19.156 'mkdir server'
