32f89a1741f385d095b0193f9bfa6d92f9698c78
Ravina Dhruve <Ravina@Ravinas-MacBook-Pro.local>
2015-11-30 00:37:00 -0500

    simple infra

--- old_env.sh
+++ old_env.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+#REDIS SERVER
 wget http://download.redis.io/releases/redis-3.0.5.tar.gz
 tar xzf redis-3.0.5.tar.gz
 cd redis-3.0.5
 make
 src/redis-server &
 
+#WEB SERVER
 cd ../
-node app.js Prod 3000 &
+curl http://npmjs.org/install.sh | sh
+npm install forever
+
+forever start app.js Prod 3000 
 
 echo "DEPLOY COMPLETED SUCCESSFULLY."
