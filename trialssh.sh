50bccdf5a6ee04aa7e4d4740258a5128ebd76443
Ravina Dhruve <Ravina@Ravinas-MacBook-Pro.local>
2015-11-30 16:16:01 -0500

    slave

--- trialssh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt-get install sshpass
-#sshpass -p rootroot ssh root@107.170.19.156
-
-#scp -r /var/lib/jenkins/jobs/Migration_monkey/workspace/new_env.sh root@107.170.19.156:new_env.sh
-
-sshpass -p 'rootroot' ssh -o StrictHostKeyChecking=no root@107.170.19.156 'git clone https://github.com/RavinaDhruve/DevOps_milestone3.git && cd DevOps_milestone3'
