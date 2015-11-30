e19e0188506249793bdab9e8ccca4769b2982861
Ravina Dhruve <Ravina@Ravinas-MacBook-Pro.local>
2015-11-30 03:37:46 -0500

    Try mig

--- trialssh.sh
+++ trialssh.sh
@@ -3,6 +3,6 @@
 sudo apt-get install sshpass
 #sshpass -p rootroot ssh root@107.170.19.156
 
-sshpass -p 'rootroot' ssh -o StrictHostKeyChecking=no root@107.170.19.156 'scp root@104.131.197.251:/var/lib/jenkins/jobs/Migration_monkey/workspace/new_env.sh migrate.sh && sh new_env.sh'
+sshpass -p 'rootroot' ssh -o StrictHostKeyChecking=no root@107.170.19.156 'scp -o StrictHostKeyChecking=no root@104.131.197.251:/var/lib/jenkins/jobs/Migration_monkey/workspace/new_env.sh migrate.sh && sh new_env.sh'
 
 #scp root@104.131.197.251:/var/lib/jenkins/jobs/Migration_monkey/workspace/migrate.sh migrate.sh
