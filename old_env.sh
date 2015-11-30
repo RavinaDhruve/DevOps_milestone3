0db10aeb9fecadaeac7427dea17c38a7f86bbee4
Ravina Dhruve <Ravina@Ravinas-MacBook-Pro.local>
2015-11-30 02:58:13 -0500

    try redis and webserver

--- old_env.sh
+++ old_env.sh
@@ -5,6 +5,10 @@ wget http://download.redis.io/releases/redis-3.0.5.tar.gz
 tar xzf redis-3.0.5.tar.gz
 cd redis-3.0.5
 make
+
+#daemonizing redis-server
+sed -i 's/daemonize no/daemonize yes/g' redis.conf
+echo "Change done"
 src/redis-server redis.conf
 
 
