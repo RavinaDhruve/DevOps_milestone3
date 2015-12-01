1eb777fe4d5476b71f0734805e4ef586e4a89fea
Ravina Dhruve <Ravina@Ravinas-MacBook-Pro.local>
2015-11-30 23:50:38 -0500

    works

--- old_env.sh
+++ old_env.sh
@@ -29,7 +29,7 @@ cd ../
 #WEB SERVER
 npm install
 sudo npm install -g forever
-forever stopall
+#forever stopall
 sudo bash -c 'forever -w start app.js Master 3000'
 
 echo "DEPLOY COMPLETED SUCCESSFULLY."
