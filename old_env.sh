8c480027a6d7d5f9ae7843e256b61303677597ed
Ravina Dhruve <Ravina@Ravinas-MacBook-Pro.local>
2015-12-01 14:08:19 -0500

    with npm

--- old_env.sh
+++ old_env.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 sudo apt-get update
+sudo apt-get install npm
+sudo npm install nodejs
+sudo apt-get install nodejs-legacy
+sudo apt-get install git
 sudo apt-get install build-essential
 sudo apt-get install tcl8.5
 
