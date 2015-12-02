ab85ebcdc73cfaf5da40879fcc34c7a078403c15
Ravina Dhruve <Ravina@Ravinas-MacBook-Pro.local>
2015-12-01 19:17:15 -0500

    sentinel

--- old_env.sh
+++ old_env.sh
@@ -18,14 +18,28 @@ cd utils
 sudo ./install_server.sh
 cd ../
 
+#sentinel-conf
+sed -i '1s/^/daemonize yes /' sentinel.conf
+sed -i 's/sentinel monitor mymaster 127.0.0.1 6379 2/sentinel monitor masterServer 107.170.51.11 6379 1/g' sentinel.conf
+sed -i 's/sentinel down-after-milliseconds mymaster 30000/sentinel down-after-milliseconds masterServer 5000/g' sentinel.conf
+sed -i 's/sentinel parallel-syncs mymaster 1/sentinel parallel-syncs masterServer 1/g' sentinel.conf
+sed -i 's/sentinel failover-timeout mymaster 180000/sentinel failover-timeout masterServer 10000/g' sentinel.conf
+
+echo "Sentinel configuration change done"
+
 #daemonizing redis-server
 #first start
 sudo service redis-server start
 
 sed -i 's/daemonize no/daemonize yes/g' redis.conf
-echo "Change done"
+echo "Redis configuration change done"
 
+#starting redis-server
 sudo service redis-server restart
+
+#starting sentinel
+sudo redis-server sentinel.conf --sentinel
+
 #src/redis-cli 'config set stop-writes-on-bgsave-error no && exit'
 cd ../
 
